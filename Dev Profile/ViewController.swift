9169436741512c9012b9589ca5ae4f3343fa8f3c
Andrii Diachenko <andreydmg@gmail.com>
2019-01-26 21:27:59 +0200

    Initial Commit

---
+++ Dev Profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dev Profile
+//
+//  Created by Andrii Diachenko on 1/26/19.
+//  Copyright © 2019 Andrii Diachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
